ac98f6dbb2e54b45f621c85862bd6cb24985bed5
unknown <aniruddhamukherjee232@gamil.com>
2023-11-29 23:34:01 +0530

    Add comment on Code for better understanding

--- src/main/java/org/example/SteamAPI/FilterWithSteamAPI.java
+++ src/main/java/org/example/SteamAPI/FilterWithSteamAPI.java
@@ -6,20 +6,22 @@ import java.util.stream.Collectors;
 
 public class FilterWithSteamAPI {
     public static void main(String[] args) {
+        /*Filter a List of String where it starts with a specific character
+        ends with a specific character*/
         List<String> names = List.of("Aniruddha", "Pritam", "Dpiankar", "Ajay", "Shivam");
         Scanner sc = new Scanner(System.in);
-        String start = sc.nextLine();
-        String end = sc.nextLine();
+        String start = sc.nextLine(); // Taking Input for Start Character
+        String end = sc.nextLine(); // Taking input for End Character
         List<String> newNames = names.stream().filter(i -> i.toLowerCase().startsWith(start.toLowerCase()) &&
-                i.toLowerCase().endsWith(end.toLowerCase())).collect(Collectors.toList());
+                i.toLowerCase().endsWith(end.toLowerCase())).collect(Collectors.toList()); // Getting the according answer
         System.out.println(newNames);
 
 
 
 
-        //Filter Integer
+        //Filter Integer List with Even Numbers
         List<Integer> numbers = List.of(10, 13, 2, 6, 8, 9, 15);
-        List<Integer> evenNumbers = numbers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+        List<Integer> evenNumbers = numbers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList()); // getting only Even Numbers
         System.out.println(evenNumbers);
     }
 }
--- src/main/java/org/example/SteamAPI/Mapping.java
+++ src/main/java/org/example/SteamAPI/Mapping.java
@@ -5,13 +5,19 @@ import java.util.stream.Collectors;
 
 public class Mapping {
     public static void main(String[] args) {
+        /*
+            Using Map Stream Convert Every String into Uppercase
+         */
         List<String> stringList = List.of("Aniruddha", "Hello World", "India");
-        List<String> newStringList = stringList.stream().map(str -> str.toUpperCase()).collect(Collectors.toList());
+        List<String> newStringList = stringList.stream().map(str -> str.toUpperCase()).collect(Collectors.toList()); // Converting into upperCase
         System.out.println(newStringList);
 
 
+        /*
+            Print the Square root of every element using Map Stream
+         */
         List<Integer> numbers = List.of(12, 10, 5, 7, 3, 16);
-        List<Integer> newNumbers = numbers.stream().map(ele -> (int)Math.sqrt(ele)).collect(Collectors.toList());
+        List<Integer> newNumbers = numbers.stream().map(ele -> (int)Math.sqrt(ele)).collect(Collectors.toList()); // Getting the Square root
         System.out.println(newNumbers);
     }
 }
