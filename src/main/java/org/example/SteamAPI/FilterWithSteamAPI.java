9d1ddbe4d341267ed631c96adfda835e0fac399c
unknown <aniruddhamukherjee232@gamil.com>
2023-11-29 17:17:35 +0530

    Uncomment a code

--- src/main/java/org/example/SteamAPI/FilterWithSteamAPI.java
+++ src/main/java/org/example/SteamAPI/FilterWithSteamAPI.java
@@ -6,13 +6,13 @@ import java.util.stream.Collectors;
 
 public class FilterWithSteamAPI {
     public static void main(String[] args) {
-//        List<String> names = List.of("Aniruddha", "Pritam", "Dpiankar", "Ajay", "Shivam");
-//        Scanner sc = new Scanner(System.in);
-//        String start = sc.nextLine();
-//        String end = sc.nextLine();
-//        List<String> newNames = names.stream().filter(i -> i.toLowerCase().startsWith(start.toLowerCase()) &&
-//                i.toLowerCase().endsWith(end.toLowerCase())).collect(Collectors.toList());
-//        System.out.println(newNames);
+        List<String> names = List.of("Aniruddha", "Pritam", "Dpiankar", "Ajay", "Shivam");
+        Scanner sc = new Scanner(System.in);
+        String start = sc.nextLine();
+        String end = sc.nextLine();
+        List<String> newNames = names.stream().filter(i -> i.toLowerCase().startsWith(start.toLowerCase()) &&
+                i.toLowerCase().endsWith(end.toLowerCase())).collect(Collectors.toList());
+        System.out.println(newNames);
 
 
 
